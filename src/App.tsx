897c05924278489ae8c75622a3991317e7039283
TRAN ANH KIET <128015630+AnhKietBme@users.noreply.github.com>
2023-10-01 20:21:23 +0900

    update

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navbar from './layouts/header-footer/Navbar';
+import Footer from './layouts/header-footer/Footer';
+import Homepage from './layouts/homepage/HomePage';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Homepage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
